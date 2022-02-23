4a9cfd12fde65e860fefb0bffb2f1d81c6cd0a8a
Joseph Martin <josephmartin1350@gmail.com>
2022-02-22 18:44:48 -0700

    commit tree 7912d846d845d75e111f859c637358494ede540a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
-
-
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :genre
+    def initialize title
+        @title = title
+    end
+    def title= title
+        @title = title
+    end
+    def author= author
+        @author = author
+    end
+    def page_count= count
+        @count = count
+    end
+    def page_count
+        @count
+    end
+    def genre= genre
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
